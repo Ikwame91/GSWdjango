290dc26eb084eda0c4ced110d41209f98b610cfa
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-03-10 23:58:19 +0000

    trying database

--- myfirstproject/main/views.py
+++ myfirstproject/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList, Item
 # Create your views here.
 
 # http request and show som stuff on our website
 
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+    ls = TodoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" %ls.title)
