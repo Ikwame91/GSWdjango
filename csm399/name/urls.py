c92c9ff419eb1533e9e5a4eaf998885ab95fb875
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-03-12 13:23:15 +0000

    adding models,insertin,deletion,and displaying data

--- csm399/name/urls.py
+++ csm399/name/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index"),
-path("kwame", views.kwame, name="kwame"),
+path("", views.names, name="names"),
+path("index/", views.index, name="index"),
+path("kwame/", views.kwame, name="kwame"),
 
 ]
--- csm399/name/views.py
+++ csm399/name/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from . models import Memeber
 
 # Create your views here.
+def names(request):
+    mynames= Memeber.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {
+        'mynames': mynames
+    }
+    return HttpResponse(template.render(context,request))
+
+
 def index(response):
-    return HttpResponse("<h1>Hello World</h1>")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
 def kwame(response):
     return HttpResponse("<h1> He's a boy who likes walking</h1>")
