86c5a5f0f7511907d1ee60e45865ad84b1873a89
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-03-13 21:01:27 +0000

    Querysets,get,filter,orderby

--- csm399/name/views.py
+++ csm399/name/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from . models import Memeber
+from django.db.models import Q
 
 # Create your views here.
 def names(request):
@@ -44,8 +45,99 @@ def testing(request):
 
 def test(request):
   mydata = Memeber.objects.values().all()
+#   Order the result alphabetically by firstname:
+#   mydata = Memeber.objects.all().order_by('firstname').values()
+  
+# Order the result firstname descending:
+#   mydata = Memeber.objects.all().order_by('-firstname').values()
+  
+  mydata = Memeber.objects.all().order_by('lastname', '-id').values()
+
+
   template = loader.get_template('test.html')
   context = {
     'mymembers': mydata,
   }
   return HttpResponse(template.render(context, request))
+
+
+
+# The filter() method takes the arguments as **kwargs (keyword arguments),
+# so you can filter on more than one field by separating them by a comma.
+# AND
+# def test(request):
+#   mydata = Memeber.objects.filter(lastname='Refsnes', id=2).values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+
+# Return the records where firstname starts with the letter 'K':
+# def test(request):
+#   mydata = Memeber.objects.filter(firstname__startswith='k').values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+# Get all records that have the value "bias" in the firstname column:
+# The contains lookup is used to get records that contains a specified value.
+# def test(request):
+#   mydata = Memeber.objects.filter(firstname__contains='bias').values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+
+
+
+# The icontains lookup is used to get records that contains a specified value.
+# The icontains lookup is case insensitive.
+# def test(request):
+#   mydata = Memeber.objects.filter(lastname__icontains='ref').values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+
+
+# Get all records where firstname ends with the letter "s":
+# mydata = Member.objects.filter(firstname__endswith='s').values()
+
+
+
+# Return records where firstname is either "Emil" or Tobias":
+# def test(request):
+#   mydata = Memeber.objects.filter(Q(firstname='Kwame') | Q(firstname='Johan')).values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+
+# mydata = Member.objects.filter(firstname='Emil').values() | Member.objects.filter(firstname='Tobias').values()
+
+# def test(request):
+#   mydata = Memeber.objects.values_list('firstname' )
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
+
+
+# def test(request):
+#   mydata = Memeber.objects.filter(firstname='Kwame').values()
+#   template = loader.get_template('test.html')
+#   context = {
+#     'mymembers': mydata,
+#   }
+#   return HttpResponse(template.render(context, request))
