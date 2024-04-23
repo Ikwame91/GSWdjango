cb5ee5a3f631c0ede1ce5cd623869f05f8517035
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-04-23 23:59:22 +0000

    revision

--- revision/tennisclub/players/views.py
+++ revision/tennisclub/players/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def players(request):
-    return HttpResponse("Helloo madafuckers.....")
+    template = loader.get_template("first.html")
+    return HttpResponse(template.render())
