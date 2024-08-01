cbe9c99b326eedefd7815638cac850f49566835b
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-08-01 11:07:04 +0000

    relearning django

--- relearning_django/my_tennis_club/my_tennis_club/settings.py
+++ relearning_django/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i@h9+-(lh7+6we3be3*dp##dr-x8_xu24htw1su2p02maq1vh^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
