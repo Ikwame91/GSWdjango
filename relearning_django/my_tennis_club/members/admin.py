32047b9df4c08f50044c1f79c4833fd4033c9315
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-08-10 23:07:50 +0000

    string __str_ wasnt working due to indentation error, now fixed"

--- relearning_django/my_tennis_club/members/admin.py
+++ relearning_django/my_tennis_club/members/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Member
 # Register your models here.
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname", "lastname","joined_date",)
-
-admin.site.register(Member,MemberAdmin)
+admin.site.register(Member)
