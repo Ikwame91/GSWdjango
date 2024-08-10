a8ff356fba3a5185e0b6768dd284de6439863d1d
Kwame Agyapong <Kwameagyapong91@gmail.com>
2024-08-10 22:38:14 +0000

    w3 schools model

--- relearning_django/my_tennis_club/members/models.py
+++ relearning_django/my_tennis_club/members/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class ToDoList(models.Model):
-    name = models.CharField(max_length=225)
-   
-    def __str__(self) :
-        return self.name
-    
-class Item(models.Model):
-    todolist = models.ForeignKey(ToDoList,on_delete=models.CASCADE)
-    text =models.CharField(max_length=225)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
